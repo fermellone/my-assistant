b93a4e513dccef6362a7b0efdac9308b3fa67783
fermellone <fernandomellone2@gmail.com>
2023-06-25 08:42:14 -0400

    removed the speech to text function

--- utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .record_audio import *
-from .save_audio_to_file import *
-from .speech_to_text import *
--- utils/save_audio_to_file.py
+++
@@ -1,9 +0,0 @@
-def save_audio_to_file(file_path: str, frames, sample_size, channels, rate):
-    import wave
-
-    wf = wave.open(file_path, "wb")
-    wf.setnchannels(channels)
-    wf.setsampwidth(sample_size)
-    wf.setframerate(rate)
-    wf.writeframes(b"".join(frames))
-    wf.close()
