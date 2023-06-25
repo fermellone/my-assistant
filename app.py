0bb4848706501b30ae4e187578176cc6119ec8f8
fermellone <fernandomellone2@gmail.com>
2023-06-25 09:26:02 -0400

    test waitress with cli

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+if app.debug:
+    import env
+
 
 @app.route("/ask", methods=["POST"])
 def ask():
@@ -16,10 +19,12 @@ def ask():
     return {"response": response}
 
 
-if __name__ == "__main__":
-    from waitress import serve
+def create_app():
+    return app
 
-    if app.debug:
-        import env
 
-    serve(app, host="0.0.0.0", port=8080)
+# if __name__ == "__main__":
+#     from waitress import serve
+
+
+#     serve(app, host="0.0.0.0", port=8080)
